1a1eecc523f6a342e2bbc2153def2e916acdae72
pancake-swap <pancakeswap1@protonmail.com>
2020-11-02 22:46:09 +0800

    update

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -9,9 +9,5 @@ const domainName = window.location.hostname;
 if (domainName.includes('localhost')) config.env = 'local';
 if (domainName === 'demo.snapshot.page' || domainName === 'beta.snapshot.page')
   config.env = 'develop';
-if (1) {
-  // @ts-ignore
-  delete config.connectors.portis;
-}
-
+delete config.connectors.portis;
 export default config;
